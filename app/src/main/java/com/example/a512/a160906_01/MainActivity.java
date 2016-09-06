06e8ec06b994d44b2b3e5db1b85bc165f3254d08
singihea <072323@naver.com>
2016-09-06 10:54:15 +0900

    16.09.06-1  버튼을 누르면 내 이름을 토스로 불러옴

---
+++ app/src/main/java/com/example/a512/a160906_01/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a512.a160906_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btnname;
+    public TextView textname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textname = (TextView) findViewById(R.id.textname);
+        btnname = (Button) findViewById(R.id.btnname);
+        btnname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textname.setText("singihea");
+                Toast.makeText(getApplicationContext(),"singihea",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
